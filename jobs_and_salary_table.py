92f4a43ae1d6c9d599fd99815a1e22cbda8859d6
Yuriy Mikhailov <apofiz2036@bk.ru>
2024-05-16 07:05:02 +0500

    united function

--- jobs_and_salary_table.py
+++ jobs_and_salary_table.py
@@ -4,7 +4,7 @@ from terminaltables import AsciiTable
 import find_jobs_and_salary as find
 
 
-def create_table_superjob(table):
+def create_table(table, title):
     table_rows = [
         [
             'Язык программирования',
@@ -23,30 +23,7 @@ def create_table_superjob(table):
         ])
 
     table = AsciiTable(table_rows)
-    table.title = 'SuperJob Moscow'
-    return table.table
-
-
-def create_table_hh(table):
-    table_rows = [
-        [
-            'Язык программирования',
-            'Вакансий найдено',
-            'Вакансий обработано',
-            'Средняя зарплата'
-        ]
-    ]
-
-    for language, vacancies in table.items():
-        table_rows.append([
-            language,
-            vacancies["vacancies_found"],
-            vacancies["vacancies_processed"],
-            vacancies["average_salary"]
-        ])
-
-    table = AsciiTable(table_rows)
-    table.title = 'HeadHunter Moscow'
+    table.title = title
     return table.table
 
 
@@ -57,5 +34,5 @@ if __name__ == '__main__':
     table_hh = find.find_jobs_on_languages_hh()
     table_superjob = find.find_jobs_on_languages_superjob(secret_key)
 
-    print(create_table_hh(table_hh))
-    print(create_table_superjob(secret_key, table_superjob))
+    print(create_table(table_hh, 'HeadHunter Moscow'))
+    print(create_table(secret_key, table_superjob, 'SuperJob Moscow'))
